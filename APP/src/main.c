c6e7dc674e5620200bdc8fbd28a1287e270f29bb
Valentin Quoniam <valentin.quoniam@ensta-paris.fr>
2021-12-16 16:17:05 +0100

    Rien changed lol

--- APP/src/main.c
+++ APP/src/main.c
@@ -474,7 +474,7 @@ int main(void)
 
     // here, touch all u like :-)
     //
-    int thresholdInfrared = 50;
+    int thresholdInfrared = 25;
     int thresholdLight = 250;
     // state will define the robot's attitude towards its environment
     // it implements a state machine
